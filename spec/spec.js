d707ea70f2e02e58203bc5771938ad48ce8c260c
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2016-03-14 12:41:56 -0400

    jasmine/selenium, chrome support

--- spec/spec.js
+++ spec/spec.js
@@ -4,26 +4,47 @@ var fs = require('fs'),
 var server = require('../server.js');
 
 var firefox = new webdriver.Builder().forBrowser('firefox').build();
+var chrome = new webdriver.Builder().forBrowser('chrome').build();
+
+function writeScreenshot(data, file) {
+	var base64Data = data.replace(/^data:image\/png;base64,/, "");
+	fs.writeFileSync(file, base64Data, 'base64');
+	console.log('wrote %s', file);
+}
 
 describe('WebVRApp', function () {
 
-	var browser;
-
-	afterEach( function (done) {
-		// take a screenshot of the page:
-		browser.takeScreenshot().then( function (data) {
-			var base64Data = data.replace(/^data:image\/png;base64,/, "");
-			var filepath = 'test/screenshots/it_opens.png';
-			fs.writeFileSync(filepath, base64Data, 'base64');
-			console.log('wrote %s', filepath);
-			done();
-		} );
-	} );
-
-	it('opens in Firefox', function () {
-		browser = firefox;
-		browser.get(server.URL);
-		console.log('it opens in Firefox!');
+	describe('inFirefox', function () {
+		afterEach( function (done) {
+			var file = 'test/screenshots/firefox/it_opens.png';
+			firefox.takeScreenshot().then( function (data) {
+				writeScreenshot(data, file);
+				done();
+			} );
+		}, 10000);
+		it('opens', function () {
+			firefox.get(server.URL);
+			console.log('it opens in Firefox!');
+		});
 	});
+
+	describe('inChrome', function () {
+		afterEach( function (done) {
+			var file = 'test/screenshots/chrome/it_opens.png';
+			chrome.takeScreenshot().then( function (data) {
+				writeScreenshot(data, file);
+				done();
+			} );
+		}, 10000);
+		it('opens', function () {
+			chrome.get(server.URL);
+			console.log('it opens in Chrome!');
+		});
+	});
+
+	afterAll( function () {
+		firefox.quit();
+		chrome.quit();
+	} );
 
 });
