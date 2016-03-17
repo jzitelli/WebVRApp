b6ee2c9c01b142b4acc5032d1c19e24900feb1f1
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2016-03-17 17:14:26 -0400

    simple keyboard input

--- WebVRKeyboard.js
+++ WebVRKeyboard.js
@@ -1,4 +1,45 @@
-function WebVRKeyboard() {
+var WebVRKeyboard = ( function () {
     "use strict";
-    // TODO
-}
+
+    function WebVRKeyboard(eventTarget, commands) {
+
+    	eventTarget.addEventListener("keydown", onKeyDown, false);
+    	eventTarget.addEventListener("keyup", onKeyUp, false);
+
+    	var keyDown = [];
+    	var commandDowns = [];
+
+    	function onKeyDown(evt) {
+    		keyDown[evt.keyCode] = true;
+    		if (commandDowns[evt.keyCode]) commandDowns[evt.keyCode]();
+    	}
+
+    	function onKeyUp(evt) {
+    		keyDown[evt.keyCode] = false;
+    	}
+
+    	function getState(buttons) {
+    		for (var i = 0; i < buttons.length; i++) {
+    			if (keyDown[buttons[i]]) return 1;
+    		}
+    		return 0;
+    	}
+
+    	for (name in commands) {
+    		var buttons = commands[name].buttons;
+            Object.defineProperty(this, name, {
+                enumerable: true,
+                get: getState.bind(this, buttons)
+            });
+    		var commandDown = commands[name].commandDown;
+    		if (commandDown) {
+    			for (var i = 0; i < buttons.length; i++) {
+	    			commandDowns[buttons[i]] = commandDown;
+	    		}
+    		}
+    	}
+
+    }
+
+    return WebVRKeyboard;
+} )();
