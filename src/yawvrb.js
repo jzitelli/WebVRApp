d2c10a440de61e6a2560edcc6c35b9f2a47c860e
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2016-05-02 16:38:39 -0400

    using browserify

---
+++ src/yawvrb.js
@@ -0,0 +1,9 @@
+window.YAWVRB = {};
+
+YAWVRB.App = require('./App.js');
+YAWVRB.AppUtils = require('./AppUtils.js');
+YAWVRB.Gamepad = require('./Gamepad.js');
+YAWVRB.GfxTablet = require('./GfxTablet.js');
+YAWVRB.Keyboard = require('./Keyboard.js');
+YAWVRB.LeapMotion = require('./LeapMotion.js');
+YAWVRB.Mouse = require('./Mouse.js');
