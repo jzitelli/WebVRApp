b81d87f45a9410e34fc1f91ee65480ff892c63bf
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2016-05-07 04:15:42 -0400

    build

--- build/yawvrb.js
+++ build/yawvrb.js
@@ -221,158 +221,6 @@ function App(scene, config, rendererOptions) {
 module.exports = App;
 
 },{}],2:[function(require,module,exports){
-/* global THREE */
-
-module.exports = ( function () {
-    "use strict";
-
-    const UP = THREE.Object3D.DefaultUp;
-    const RIGHT = new THREE.Vector3(1, 0, 0);
-
-    function ObjectSelector() {
-
-        var selectables = [];
-
-        this.selection;
-        var heading = 0;
-        var pitch = 0;
-
-        this.addSelectable = function (obj) {
-            selectables.push(obj);
-            if (!this.selection) this.selection = obj;
-        }.bind(this);
-
-        this.cycleSelection = ( function () {
-            var i = 0;
-            var euler = new THREE.Euler();
-            return function () {
-                i = (i + 1) % selectables.length;
-                this.selection = selectables[i];
-                euler.setFromQuaternion(this.selection.quaternion);
-                heading = euler.y;
-                pitch = euler.x;
-            };
-        } )().bind(this);
-
-        const MOVESPEED = 0.3;
-        var pitchQuat = new THREE.Quaternion();
-
-        this.moveSelection = function (dt, moveFB, moveRL, moveUD, turnRL, turnUD) {
-            var selection = this.selection;
-            if (!selection) return;
-            if (moveFB || moveRL || moveUD || turnRL || turnUD) {
-                heading -= (turnRL) * dt;
-                pitch   -= (turnUD) * dt;
-                var cos = Math.cos(heading),
-                    sin = Math.sin(heading);
-                selection.position.z -= dt * MOVESPEED * ((moveFB) * cos + (moveRL) * sin);
-                selection.position.x += dt * MOVESPEED * ((moveRL) * cos - (moveFB) * sin);
-                selection.position.y += dt * MOVESPEED * moveUD;
-                selection.quaternion.multiplyQuaternions(selection.quaternion.setFromAxisAngle(UP, heading), pitchQuat.setFromAxisAngle(RIGHT, pitch));
-                selection.updateMatrix();
-                selection.updateMatrixWorld();
-            }
-        }.bind(this);
-
-        this.saveAllTransforms = function (key) {
-            if (!window.localStorage) {
-                console.error('platform does not support localStorage');
-                return;
-            }
-            key = key || 'YAWVRB_TRANSFORMS';
-            var transforms = {};
-            selectables.forEach( function (object) {
-                if (object.name) {
-                    transforms[object.name] = {
-                        position: object.position.toArray(),
-                        quaternion: object.quaternion.toArray()
-                    };
-                }
-            } );
-            window.localStorage[key] = transforms;
-        };
-
-        this.loadTransforms = function (key) {
-            if (!window.localStorage) {
-                console.error('platform does not support localStorage');
-                return;
-            }
-            key = key || 'YAWVRB_TRANSFORMS';
-            var transforms = window.localStorage[key];
-            selectables.forEach( function (object) {
-                if (object.name && transforms[object.name]) {
-                    var transform = transforms[object.name];
-                    object.position.fromArray(transform.position);
-                    object.quaternion.fromArray(transform.quaternion);
-                    object.updateMatrix();
-                }
-            } );
-        };
-    }
-
-    var DEADSCENE = new THREE.Scene();
-    DEADSCENE.name = 'DEADSCENE';
-    var displayText = ( function () {
-        var textMeshes = {};
-        var quadGeom = new THREE.PlaneBufferGeometry(1, 1);
-        quadGeom.translate(0.5, 0.5, 0);
-        const DEFAULT_OPTIONS = {
-            object: DEADSCENE,
-            position: [0, 0.05, -0.05],
-            quaternion: [0, 0, 0, 1],
-            coordSystem: 'local',
-            textSize: 21
-        };
-        function displayText(text, options) {
-            options = options || {};
-            for (var kwarg in DEFAULT_OPTIONS) {
-                if (options[kwarg] === undefined) options[kwarg] = DEFAULT_OPTIONS[kwarg];
-            }
-            var uuid = options.object.uuid;
-            var key = JSON.stringify({text, uuid});
-            var mesh = textMeshes[key];
-            if (!mesh) {
-                var canvas = document.createElement('canvas');
-                canvas.height = 2 * options.textSize;
-                canvas.width = 256; //2*ctx.measureText(text).width;
-                var ctx = canvas.getContext('2d');
-                ctx.font = String(options.textSize) + "px serif";
-                // ctx.fillStyle   = 'rgba(23, 23, 23, 0.3)';
-                // ctx.strokeStyle = 'rgba(23, 23, 23, 0.3)';
-                // ctx.fillRect(  0, 0, canvas.width, canvas.height);
-                // ctx.strokeRect(0, 0, canvas.width, canvas.height);
-                ctx.fillStyle   = 'rgb(255, 72, 23)';
-                ctx.strokeStyle = 'rgb(240, 70, 20)';
-                ctx.fillText(  text, 0, options.textSize);
-                ctx.strokeText(text, 0, options.textSize);
-                var aspect = canvas.width / canvas.height;
-                var texture = new THREE.Texture(canvas, THREE.UVMapping, THREE.ClampToEdgeWrapping, THREE.ClampToEdgeWrapping, THREE.LinearFilter, THREE.LinearFilter);
-                var material = new THREE.MeshBasicMaterial({color: 0xffffff, map: texture, transparent: true});
-                mesh = new THREE.Mesh(quadGeom, material);
-                material.map.needsUpdate = true;
-                if (options.coordSystem === 'local') {
-                    options.object.add(mesh);
-                    mesh.position.fromArray(options.position);
-                    mesh.quaternion.fromArray(options.quaternion);
-                    var worldScale = options.object.getWorldScale();
-                    mesh.scale.set(aspect * 0.075 / worldScale.x, 0.075 / worldScale.y, 1 / worldScale.z);
-                    mesh.updateMatrix();
-                }
-                textMeshes[key] = mesh;
-            }
-        }
-        return displayText;
-    } )();
-
-    return {
-        ObjectSelector: ObjectSelector,
-        displayText: displayText,
-        DEADSCENE: DEADSCENE
-    };
-
-} )();
-
-},{}],3:[function(require,module,exports){
 module.exports = ( function () {
     "use strict";
 
@@ -494,7 +342,7 @@ module.exports = ( function () {
 
 } )();
 
-},{}],4:[function(require,module,exports){
+},{}],3:[function(require,module,exports){
 /* global THREE, GFXTABLET */
 
 module.exports = ( function () {
@@ -523,7 +371,7 @@ module.exports = ( function () {
 	return GfxTablet;
 } )();
 
-},{}],5:[function(require,module,exports){
+},{}],4:[function(require,module,exports){
 /* global THREE */
 
 module.exports = ( function () {
@@ -828,7 +676,7 @@ module.exports = ( function () {
     return Keyboard;
 } )();
 
-},{}],6:[function(require,module,exports){
+},{}],5:[function(require,module,exports){
 /* *********************************************************************************************
 
    To connect to remote Leap Motion controllers, add this to the host's Leap Motion config.json:
@@ -1255,7 +1103,7 @@ module.exports = ( function () {
 
 } )();
 
-},{}],7:[function(require,module,exports){
+},{}],6:[function(require,module,exports){
 /* global THREE */
 
 module.exports = ( function () {
@@ -1299,15 +1147,192 @@ module.exports = ( function () {
 	return Mouse;
 } )();
 
+},{}],7:[function(require,module,exports){
+module.exports = ( function () {
+    "use strict";
+
+    function Stage() {
+        this.hmdCalibrationPose = null;
+        this.deskHeight = 29 * 0.0254;
+        this.sensors = {};
+        this.objects = [];
+
+        var vrDisplay;
+
+        navigator.getVRDisplays().then( function (displays) {
+            for (var i = 0; i < displays.length; i++) {
+                vrDisplay = displays[i];
+                console.log('vrDisplay:\n' + vrDisplay);
+                if (vrDisplay.stageParameters && vrDisplay.stageParameters.sittingToStandingTransform) {
+                    console.log('sittingToStandingTransform:\n' + vrDisplay.stageParameters.sittingToStandingTransform);
+                } else {
+                    console.warn('no sittingToStandingTransform provided by the VRDisplay');
+                }
+            }
+        } );
+
+        this.updateCalibrationPose = function ( pose, t ) {
+            if (pose) console.log(pose);
+            if (t) console.log(t);
+        }.bind(this);
+
+        this.save = function (key) {
+            key = key || 'StagePoses';
+            var transforms = {};
+            this.objects.forEach( function (object) {
+                if (object.name) {
+                    transforms[object.name] = {
+                        position: object.position.toArray(),
+                        quaternion: object.quaternion.toArray()
+                    };
+                }
+            } );
+            window.localStorage[key] = transforms;
+        }.bind(this);
+
+        this.load = function (key) {
+            key = key || 'StagePoses';
+            var transforms = window.localStorage[key];
+            this.objects.forEach( function (object) {
+                if (object.name && transforms[object.name]) {
+                    var transform = transforms[object.name];
+                    object.position.fromArray(transform.position);
+                    object.quaternion.fromArray(transform.quaternion);
+                    object.updateMatrix();
+                    object.updateMatrixWorld();
+                }
+            } );
+        }.bind(this);
+
+    }
+
+    return Stage;
+} )();
+
 },{}],8:[function(require,module,exports){
+/* global THREE */
+
+module.exports = ( function () {
+    "use strict";
+
+    const UP = THREE.Object3D.DefaultUp;
+    const RIGHT = new THREE.Vector3(1, 0, 0);
+
+    var moveObject = ( function () {
+        const MOVESPEED = 0.3;
+        var euler = new THREE.Euler();
+        var pitchQuat = new THREE.Quaternion();
+        return function (object, dt, moveFB, moveRL, moveUD, turnRL, turnUD) {
+            if (moveFB || moveRL || moveUD || turnRL || turnUD) {
+                euler.setFromQuaternion(object.quaternion);
+                var heading = euler.y;
+                var pitch = euler.x;
+                heading -= (turnRL) * dt;
+                pitch   -= (turnUD) * dt;
+                var cos = Math.cos(heading),
+                    sin = Math.sin(heading);
+                object.position.z -= dt * MOVESPEED * ((moveFB) * cos + (moveRL) * sin);
+                object.position.x += dt * MOVESPEED * ((moveRL) * cos - (moveFB) * sin);
+                object.position.y += dt * MOVESPEED * moveUD;
+                if (pitch) pitchQuat.setFromAxisAngle(RIGHT, pitch);
+                object.quaternion.multiplyQuaternions(object.quaternion.setFromAxisAngle(UP, heading), pitchQuat);
+                object.updateMatrix();
+                object.updateMatrixWorld();
+            }
+        };
+    } )();
+
+    function ObjectSelector() {
+        this.selection;
+        var selectables = [];
+
+        this.addSelectable = function (obj) {
+            selectables.push(obj);
+            if (!this.selection) this.selection = obj;
+        }.bind(this);
+
+        this.cycleSelection = ( function () {
+            var i = 0;
+            return function () {
+                i = (i + 1) % selectables.length;
+                this.selection = selectables[i];
+            };
+        } )().bind(this);
+    }
+
+    var DEADSCENE = new THREE.Scene();
+    DEADSCENE.name = 'DEADSCENE';
+    var displayText = ( function () {
+        var textMeshes = {};
+        var quadGeom = new THREE.PlaneBufferGeometry(1, 1);
+        quadGeom.translate(0.5, 0.5, 0);
+        const DEFAULT_OPTIONS = {
+            object: DEADSCENE,
+            position: [0, 0.05, -0.05],
+            quaternion: [0, 0, 0, 1],
+            coordSystem: 'local',
+            textSize: 21
+        };
+        function displayText(text, options) {
+            options = options || {};
+            for (var kwarg in DEFAULT_OPTIONS) {
+                if (options[kwarg] === undefined) options[kwarg] = DEFAULT_OPTIONS[kwarg];
+            }
+            var uuid = options.object.uuid;
+            var key = JSON.stringify({text, uuid});
+            var mesh = textMeshes[key];
+            if (!mesh) {
+                var canvas = document.createElement('canvas');
+                canvas.height = 2 * options.textSize;
+                canvas.width = 256; //2*ctx.measureText(text).width;
+                var ctx = canvas.getContext('2d');
+                ctx.font = String(options.textSize) + "px serif";
+                // ctx.fillStyle   = 'rgba(23, 23, 23, 0.3)';
+                // ctx.strokeStyle = 'rgba(23, 23, 23, 0.3)';
+                // ctx.fillRect(  0, 0, canvas.width, canvas.height);
+                // ctx.strokeRect(0, 0, canvas.width, canvas.height);
+                ctx.fillStyle   = 'rgb(255, 72, 23)';
+                ctx.strokeStyle = 'rgb(240, 70, 20)';
+                ctx.fillText(  text, 0, options.textSize);
+                ctx.strokeText(text, 0, options.textSize);
+                var aspect = canvas.width / canvas.height;
+                var texture = new THREE.Texture(canvas, THREE.UVMapping, THREE.ClampToEdgeWrapping, THREE.ClampToEdgeWrapping, THREE.LinearFilter, THREE.LinearFilter);
+                var material = new THREE.MeshBasicMaterial({color: 0xffffff, map: texture, transparent: true});
+                mesh = new THREE.Mesh(quadGeom, material);
+                material.map.needsUpdate = true;
+                if (options.coordSystem === 'local') {
+                    options.object.add(mesh);
+                    mesh.position.fromArray(options.position);
+                    mesh.quaternion.fromArray(options.quaternion);
+                    var worldScale = options.object.getWorldScale();
+                    mesh.scale.set(aspect * 0.075 / worldScale.x, 0.075 / worldScale.y, 1 / worldScale.z);
+                    mesh.updateMatrix();
+                }
+                textMeshes[key] = mesh;
+            }
+        }
+        return displayText;
+    } )();
+
+    return {
+        ObjectSelector: ObjectSelector,
+        displayText: displayText,
+        moveObject: moveObject,
+        DEADSCENE: DEADSCENE
+    };
+
+} )();
+
+},{}],9:[function(require,module,exports){
 window.YAWVRB = {};
 
 window.YAWVRB.App        = require('./App.js');
-window.YAWVRB.AppUtils   = require('./AppUtils.js');
+window.YAWVRB.Utils      = require('./Utils.js');
 window.YAWVRB.Gamepad    = require('./Gamepad.js');
 window.YAWVRB.GfxTablet  = require('./GfxTablet.js');
 window.YAWVRB.Keyboard   = require('./Keyboard.js');
 window.YAWVRB.LeapMotion = require('./LeapMotion.js');
 window.YAWVRB.Mouse      = require('./Mouse.js');
+window.YAWVRB.Stage      = require('./Stage.js');
 
-},{"./App.js":1,"./AppUtils.js":2,"./Gamepad.js":3,"./GfxTablet.js":4,"./Keyboard.js":5,"./LeapMotion.js":6,"./Mouse.js":7}]},{},[8]);
+},{"./App.js":1,"./Gamepad.js":2,"./GfxTablet.js":3,"./Keyboard.js":4,"./LeapMotion.js":5,"./Mouse.js":6,"./Stage.js":7,"./Utils.js":8}]},{},[9]);
